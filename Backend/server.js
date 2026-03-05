36c8ef4f5c0ba702416b913cdc282520f1823742
NicoMaker <nicola.marano02@gmail.com>
2026-03-05 10:01:23 +0100

    dd

--- Backend/server.js
+++ Backend/server.js
@@ -18,7 +18,7 @@ const utentiRoutes = require("./routes/utenti");
 // ========================================
 // 🌐 CONFIGURAZIONE VPS
 // ========================================
-const PORT = process.env.PORT || 3005;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 // ========================================
